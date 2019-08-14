3889b4a02f75ea92fbfd9af49c63862d0f430da3
rzylber <ricardo@digitao.com.br>
2019-08-14 11:13:15 -0300

    Pag4

--- Fase1.gd
+++ Fase1.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var paginas = ["Pag1", "Pag2", "Pag3"]
+var paginas = ["Pag1", "Pag2", "Pag3", "Pag4"]
 var paginaAtual = 0
 
 func _ready():
@@ -46,3 +46,35 @@ func _on_Button2_button_up():
 
 func _on_Itchio_button_up():
 	OS.shell_open("https://itch.io/")
+
+
+func _on_PaperPlease_button_up():
+	OS.shell_open("https://youtu.be/_QP5X6fcukM")
+
+
+func _on_Showcase1_button_up():
+	OS.shell_open("https://godotengine.org/showcase")
+
+
+func _on_Showcase2_button_up():
+	OS.shell_open("https://www.youtube.com/watch?v=UTAeDoRIHaA")
+
+
+func _on_Showcase3_button_up():
+	OS.shell_open("https://www.youtube.com/watch?v=NlKEO1N8wMM")
+
+
+func _on_Showcase4_button_up():
+	OS.shell_open("https://www.youtube.com/watch?v=JF60CfSLjew")
+
+
+func _on_Download_button_up():
+	OS.shell_open("https://godotengine.org/download")
+
+
+func _on_Docs_button_up():
+	OS.shell_open("https://docs.godotengine.org/en/3.1/index.html")
+
+
+func _on_Exemplos_button_up():
+	OS.shell_open("https://github.com/godotengine/godot-demo-projects")
